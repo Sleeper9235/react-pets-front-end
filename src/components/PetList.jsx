9a58e6acd9ff3452169d78932de39b4f342afad0
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-23 15:19:09 -0500

    mvp met

--- src/components/PetList.jsx
+++ src/components/PetList.jsx
@@ -1,14 +1,19 @@
 const PetList = (props) => {
     const pets = props.petList.map((pet) => (
-        <a key={pet._id} onClick={() => [props.updateSelected(pet), console.log(pet)]}>
+        <a key={pet._id} onClick={() => props.updateSelected(pet)}>
           <li>{pet.name}</li>
         </a>
       ));
 
     return (
-        <div>
+        <div className="sidebar-container">
           <h1>Pet List</h1>
+          <div className="list-container">
           {!props.petList.length ? <h2>No Pets Yet!</h2>: <ul>{pets}</ul>}
+          </div>
+          <button onClick={props.handleFormView}>
+            {props.isFormOpen ? ('Close Form') : ('New Pet')}
+          </button>
         </div>
       );
 };
