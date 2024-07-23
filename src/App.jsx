87cc55a82a9222947856b21c6d1caec2d0ac6c6f
Sleeper9235 <kylejyoumans@gmail.com>
2024-07-23 12:06:34 -0500

    PetDetails complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 // src/App.jsx
+import { useState, useEffect } from 'react';
+import * as petService from './services/petServices';
+
+import PetList from './components/PetList';
+import PetDetails from './components/PetDetails';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [petList, setPetList] = useState([])
+  const [selected, setSelected] = useState(null)
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const pets = await petService.index()
+
+        if (pets.err) {
+          throw new Error(pets.err)
+        }
+  
+        setPetList(pets)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchPets()
+  }, [])
+
+  const updateSelected = (pet) => {
+    setSelected(pet)
+  }
+
+  return (
+  <>
+    <PetList petList={petList} updateSelected={updateSelected}/>
+    <PetDetails selected={selected} />
+  </>
+  )
 };
 
 export default App;
